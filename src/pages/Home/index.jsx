7043cca79a5ca329d1768b267b2132d1d8f1a3f5
RobertoJofreGodoy <roberto.jofre.godoy.dev@gmail.com>
2021-07-12 18:26:44 -0400

    buscador básico

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,43 @@
-import { Link } from "wouter"
+import { useState } from "react";
+import { Link, useLocation } from "wouter";
 
-const POPULAR_GIFS = [ 'Matrix', "Witcher", "Star Wars", "House"]
+const POPULAR_GIFS = ["Matrix", "Witcher", "Star Wars", "House"];
 
 export default function Home() {
-    return (
-        <>
-            <h3>Los gifs más populares</h3>
-            <ul>
-                {
-                    POPULAR_GIFS.map( (popularGif) => (
-                        <li key={popularGif}>
-                            <Link to={`/search/${popularGif}`} >Gifs de {popularGif}</Link>
-                        </li>
-                    ))
-                }
-            </ul>
-            
-        </>
-    )
+  const [keyword, setKeyword] = useState("")
+
+  //custom-hook de wouter
+  const [ path, pushLocation ] = useLocation()
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    // navegar otra ruta
+    pushLocation(`/search/${keyword}`)
+  }
+
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value)
+  }
+    
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={keyword}
+          onChange={handleChange}
+        />
+      </form>
+
+      <h3>Los gifs más populares</h3>
+      <ul>
+        {POPULAR_GIFS.map((popularGif) => (
+          <li key={popularGif}>
+            <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
