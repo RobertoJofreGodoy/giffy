8e78f28261827f3335711e0f6cf55bbdcd0f781c
RobertoJofreGodoy <roberto.jofre.godoy.dev@gmail.com>
2021-07-12 17:24:49 -0400

    vista de cada Gif

---
+++ src/components/Gif.jsx
@@ -0,0 +1,8 @@
+export default function Gif( { id, title, url }) {
+  return (
+    <a key={id} className="gif" href={`#${id}`}>
+      <h4>{title}</h4>
+      <img src={url} alt={title} className="gif__image" />
+    </a>
+  );
+}
