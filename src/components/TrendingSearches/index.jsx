d3cc2b92230f6181662aa6b6c2846ae6e9083be6
RobertoJofreGodoy <roberto.jofre.godoy.dev@gmail.com>
2021-07-13 11:36:26 -0400

    Lazy Load of Trends

--- src/components/TrendingSearches/index.jsx
+++ src/components/TrendingSearches/index.jsx
@@ -1,19 +1,46 @@
-import React from "react"
-import getTrendingTerms from '../../services/getTrendingTermsService'
-import Category from '../Category'
+import React, { useEffect } from "react";
+import getTrendingTerms from "../../services/getTrendingTermsService";
+import Category from "../Category";
 
-export default function TrendingSearches() {
+function TrendingSearches() {
+  const [trends, setTrends] = React.useState([]);
 
-    const [ trends, setTrends ] = React.useState([])
+  React.useEffect(() => {
+    getTrendingTerms().then(setTrends);
+  }, []);
 
-    React.useEffect( () => {
-        getTrendingTerms()
-            .then( setTrends)
+  return (
+    <div>
+      <Category name="tendencias" options={trends} />
+    </div>
+  );
+}
+
+export default function LazyTrending () {
+
+    const [ show, setShow] = React.useState(false)
+
+    useEffect( () => {
+
+        const onChange = ( entries ) => {
+            const el = entries[0]
+            
+            if (el.isIntersecting) {
+                setShow(true)
+            }
+        }
+
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px',
+        })
+
+        observer.observe(document.getElementById('LaziTrending'))
     }, [])
 
     return (
-        <div>
-            <Category name="tendencias" options={trends}/>
+        <div id="LaziTrending">
+            { show ?  <TrendingSearches /> : null }
         </div>
     )
+
 }
